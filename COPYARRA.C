1a4449ebc4771dee25355442be19dc6293d71535
Pranav G. Lolage <129136237+Pranavlolage00@users.noreply.github.com>
2023-06-28 18:15:21 +0530

    Add files via upload

---
+++ COPYARRA.C
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int a[100],b[100],i,n;
+  clrscr();
+  printf("enter limit");
+  scanf("%d",&n);
+  printf("enter n numbers in an array");
+  for(i=0; i<n; i++)
+  {
+   scanf("%d",&a[i]);
+  }
+  for(i=0; i<n; i++)
+  {
+    b[i]=a[i];
+  }
+  printf("first array");
+  for(i=0; i<n; i++)
+  {
+    printf("%d\t",a[i]);
+  }
+  printf("second array");
+  for(i=0; i<n; i++)
+  {
+    printf("%d\t",b[i]);
+  }
+  getch();
+  return 0;
+
+
+
+
+}
