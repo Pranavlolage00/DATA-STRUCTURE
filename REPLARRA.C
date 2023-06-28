111c0dacf82c863ed2ed3a2a6fbabf7a7047ff1e
Pranav G. Lolage <129136237+Pranavlolage00@users.noreply.github.com>
2023-06-28 19:19:11 +0530

    Add files via upload

---
+++ REPLARRA.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+   int a[100],n,i,x,y;
+   clrscr();
+   printf("enter limit :_");
+   scanf("%d",&n);
+   printf("enter n numbers :-");
+   for(i=0; i<n; i++)
+   {
+    scanf("%d",&a[i]);
+   }
+   printf("enter number to find:- ");
+   scanf("%d",&x);
+   printf("enter number to replace:-");
+   scanf("
+}
