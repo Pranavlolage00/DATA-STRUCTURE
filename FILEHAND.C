0924d2bf0879d7155e7f5496cfb78104ad1f4c28
Pranav G. Lolage <129136237+Pranavlolage00@users.noreply.github.com>
2023-07-26 07:51:18 +0530

    Add files via upload

---
+++ FILEHAND.C
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+int main()
+{
+   FILE *fp1;
+   int num;
+   clrscr();
+   fp1=fopen("numbers.txt","r");
+    if(fp1==NULL)
+    {
+      printf("file not found");
+      exit(0);
+    }
+      while(!feof(fp1))
+       {
+	 fscanf(fp1,"%d",&num);
+	 printf("%d\n",num);
+
+       }
+      fclose(fp1);
+    getch();
+    return 0;
+}
