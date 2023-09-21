51634c6aa80e4b7ad99c04b5dcb806ebff712f76
Pranav G. Lolage <129136237+Pranavlolage00@users.noreply.github.com>
2023-09-21 22:00:34 +0530

    Add files via upload

--- doubly_circular.c
+++ doubly_circular.c
@@ -59,13 +59,72 @@ void disp(node *list)
     
     return list;
  }
+ node* insertcom(node *list,int num,int pos)
+ {
+    node *temp,*newnode;
+    int i;
+    newnode=NODEALLOC;
+    newnode->data=num;
+    if(pos==1)
+    { 
+       for(temp=list; temp->next!=list; temp=temp->next);
+       newnode->next=list;
+       list->prev=newnode;
+       list=newnode;
+       temp->next=list;
+       list->prev=temp;  
+    }
+    else
+    {
+       for(i=1,temp=list;i<pos-1&&temp->next!=list; i++,temp=temp->next);
+       newnode->next=temp->next;
+       temp->next->prev=newnode;
+       temp->next=newnode;
+       newnode->prev=temp; 
+    }
+    return list;
+ }
+ node* deletebig(node *list)
+ {
+    node *temp,*temp1;
+    for(temp=list; temp->next!=list; temp=temp->next);
+    temp1=list;
+    list=list->next;
+    temp->next=list;
+    list->prev=temp;
+    free(temp1);
+    return list;
+ }
+ node* deletecom(node *list,int pos)
+ {
+    node *temp,*temp1;
+    int i;
+    if(pos==1)
+    {
+    for(temp=list; temp->next!=list; temp=temp->next);
+    temp1=list;
+    list=list->next;
+    temp->next=list;
+    list->prev=temp;
+    free(temp1);
+    }
+    else
+    {
+        for(i=1,temp=list; i<pos-1&&temp->next!=list; i++,temp=temp->next);
+        temp1=temp->next;
+        temp->next=temp1->next;
+        temp1->next->prev=temp;
+        free(temp1);
+    }
+    return list;
+ }
 int main()
 {
     node *list=NULL;
-    int ch,num;
+    int ch,num,pos;
     do
     {
-      printf("\n 1- create \n 2- display \n  3- insert bigining \n");
+      printf("\n 1- create \n 2- display \n  3- insert bigining \n 4- insert position \n 5- delete bigining \n6- delete position \n");
       printf("enter choice:");
       scanf("%d",&ch);
       switch(ch)
@@ -78,6 +137,19 @@ int main()
                 scanf("%d",&num);
                list=insertbig(list,num);
                break;
+        case 4:printf("enter position:");
+               scanf("%d",&pos);
+               printf("enter number:");
+               scanf("%d",&num);
+               list=insertcom(list,num,pos);
+               break;
+        case 5:list=deletebig(list);
+               break;
+        case 6:printf("enter position:");
+               scanf("%d",&pos);
+               list=deletecom(list,pos);
+               break;
+               
       }
-    }while(ch<4);
+    }while(ch<7);
 }
